7d70755eacffb72c30121f819417aa1e4483e418
Mehedi Hasan <m3h3d1ha2an@proton.me>
2025-12-07 08:49:24 +0600

    added better auth recommended proxy

---
+++ src/proxy.ts
@@ -0,0 +1,38 @@
+import { getSessionCookie } from "better-auth/cookies";
+import { type NextRequest, NextResponse } from "next/server";
+
+const protectedRoutes = ["/", "/profile"];
+
+export const proxy = (request: NextRequest) => {
+  const pathname = request.nextUrl.pathname;
+  const sessionCookie = getSessionCookie(request);
+  
+  const isAuthenticated = !!sessionCookie;
+  const isProtectedRoute = protectedRoutes.includes(pathname);
+  const isAuthRoute = pathname.startsWith("/auth");
+  
+  const response = NextResponse.next();
+
+  if (isProtectedRoute && !isAuthenticated) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  if (isAuthRoute && isAuthenticated) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return response;
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
